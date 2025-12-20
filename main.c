02f51fd79a135de12fee9cb0051dc141f60629f3
RobinPC <robin.walz.wien@gmail.com>
2025-12-20 13:04:22 +0100

    Added base code

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+typedef struct letternode {
+    char letter;
+    struct letternode* next;
+} LetterNode;
+
+typedef struct wordnode {
+    LetterNode* word;
+    struct wordnode* next;
+} WordNode;
+
+int compare_alphabethically(LetterNode* m, LetterNode* n) {
+    int valueM = m != NULL ? m->letter : 0; // ASCII value of the letter
+    int valueN = n != NULL ? n->letter : 0; // or 0 when node is NULL
+    int diff = valueM - valueN;             // indicates which letter is the higher value
+
+    // on equal value
+    if (diff == 0 && m != NULL && n != NULL) {
+        // continue recursively
+        return compare_alphabethically(m->next, n->next);
+    }
+
+    return diff;
 }
